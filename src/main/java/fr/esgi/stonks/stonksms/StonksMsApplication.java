10d8ec1ccd84b204dbe9ab15dd97ce50c9dcd5c9
Gwendal <gwendou95@hotmail.fr>
2021-05-30 21:51:31 +0200

    Initial commit

---
+++ src/main/java/fr/esgi/stonks/stonksms/StonksMsApplication.java
@@ -0,0 +1,13 @@
+package fr.esgi.stonks.stonksms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StonksMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StonksMsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/esgi/stonks/stonksms/StonksMsApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.esgi.stonks.stonksms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StonksMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
